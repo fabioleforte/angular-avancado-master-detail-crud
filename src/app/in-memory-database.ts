2234840580e7fff4c085eda84eea9ec96636f895
Fabio <fabio.leforte@gmail.com>
2020-04-26 03:55:32 -0300

    adiciondo category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,10 +1,11 @@
+import { Category } from './pages/categories/shared/category.model';
 import { InMemoryDbService } from '@angular-in-memory-web-api';
 
 export class InMemoryDataBase implements InMemoryDataBase {
 
   createDb() {
 
-    const categories = [
+    const categories: Category[] = [
       { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
       { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios' },
       { id: 3, name: 'Lazer', description: 'Cinema, parque, praia, etc' },
